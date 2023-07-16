98c78f7b5d2db557c632ddde65564d40941bc626
yohan <koyohanyoko@gmail.com>
2023-07-16 16:22:56 +0900

    portfolio 0716

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -43,9 +43,9 @@ const About = () => {
                     <Col className="about-icons">
                       <h1 className="yellow about-h">doing these days ...</h1>
                       <p>ㆍ학습내용 복습 및 velog에 정리<span className="about-span"> - ing</span></p>
+                      <p>ㆍStudying React / Coding Test <span className="about-span"> - ing</span></p>
                       <p>ㆍ정보처리기사 공부<span className="about-span"> - 23.06.14 필기 합격 / 실기 준비 中</span></p>
-                      <p>ㆍNomadcoders Challenge JS<span className="about-span"> - 23.05.29 수료</span></p>
-                      <p>ㆍNomadcoders Challenge TS<span className="about-span"> - 23.06.19 수료</span></p>
+                      <p>ㆍNomadcoders Challenge JS / TS<span className="about-span"> - 05.29 / 06.19 수료</span></p>
                     </Col>
                   </Col>
                 </Col>
